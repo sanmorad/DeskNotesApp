f019a218178f13c39487d604ceb3e16bf4093eb5
Adrian <adri95sanz@gmail.com>
2018-04-06 14:29:34 +0200

    Inicio

---
+++ DeskNotesApp/DeskNotesApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DeskNotesApp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new DeskNotesApp.App());
+        }
+    }
+}
---
+++ DeskNotesApp/DeskNotesApp/View/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DeskNotesApp.View
+{
+	public partial class MainPage : MasterDetailPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                MasterBehavior = MasterBehavior.Popover;
+            }
+        }
+	}
+}
